80cb9daef9ed8e20ebb060f7d786c3054313fbdd
aura <git@auravoid.dev>
2023-12-05 10:44:48 -0500

    Day 1 Part 2

--- day-1/index.ts
+++ day-1/index.ts
@@ -8,7 +8,11 @@ const calValues: string[] = [];
 let sum = 0;
 
 lines.forEach((line) => {
-    line = line.replace(/[a-z]/g, "");
+    line = line.replace(/one/g, "o1e").replace(/two/g, "t2o").replace(/three/g, "t3ree").replace(/four/g, "f4ur").replace(/five/g, "f5ve").replace(/six/g, "s6x").replace(/seven/g, "s7ven").replace(/eight/g, "e8ght").replace(/nine/g, "n9ne");
+    let words = line.match(/(one|two|three|four|five|six|seven|eight|nine|\d)/g)
+
+    line = words?.join('') as string;
+    line = line.replace(/[a-z]/g, "")
     const first = line[0];
     const last = line[line.length - 1];
     const calValue = `${first}${last}`;
